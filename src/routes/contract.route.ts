1fe6d9445278c03678390cfe440470994f8b6017
Alkapandelis <alexkpnd@gmail.com>
2026-01-10 02:14:29 +0200

    Feat: add contracts routes

--- src/routes/contract.route.ts
+++ src/routes/contract.route.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import * as contractCtrl from '../controllers/contract.controller';
+import { authenticate } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-
-router.get('/', contractCtrl.findAllContracts);
-//router.post('/', contractCtrl.createNewContract);
-
+// have to add validate zod schemas
+router.get('/', authenticate, contractCtrl.findAllContracts);
+router.post('/',authenticate, contractCtrl.createNewContract);
+//router.put('/', authenticate, contractCtrl.updateContract); 
+//router.delete('/', authenticate, contractCtrl.removeContract); //admin delete
 
 export default router;
