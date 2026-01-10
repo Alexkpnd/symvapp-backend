5c7ef95decca291c84415d60176aa759532ad6f1
Alkapandelis <alexkpnd@gmail.com>
2026-01-10 02:13:05 +0200

    Feat: add more routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,6 +2,7 @@ import { Router} from 'express';
 import * as authCtrl from "../controllers/auth.controller";
 import { validate } from '../middlewares/validate.middleware';
 import { loginSchema, registerSchema } from '../validators/auth.validator';
+import { authenticate } from '../middlewares/auth.middleware';
 
 
 
@@ -10,5 +11,6 @@ const router = Router();
 
 router.post('/login', validate(loginSchema), authCtrl.login);
 router.post('/register', validate(registerSchema), authCtrl.register);
+router.get('/me',authenticate, authCtrl.me)
 
 export default router;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,17 +2,19 @@ import { Router } from 'express';
 import * as userCtrl from "../controllers/user.controller";
 import { validate } from '../middlewares/validate.middleware';
 import { createUserSchema, updateUserSchema } from '../validators/user.validator';
+import { validateObjId } from '../middlewares/validateObjId.middleware';
+import { authenticate } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 
 
 
-router.get('/', userCtrl.getAllUsers);
-router.post('/', validate(createUserSchema), userCtrl.createUser); //admin create
-router.put('/:id', validate(updateUserSchema), userCtrl.updateUser); // admin update
-router.put('/update/:email', validate(updateUserSchema), userCtrl.updateUserByEmail); //alternative update.
-router.delete('/:id', userCtrl.removeUserById); // admin delete
+router.get('/', authenticate, userCtrl.getAllUsers);
+router.post('/', authenticate, validate(createUserSchema), userCtrl.createUser); //admin create
+router.put('/:id',authenticate, validateObjId('id'), validate(updateUserSchema),  userCtrl.updateUser); // admin update or not
+//router.put('/update/:email', authenticate, validate(updateUserSchema), userCtrl.updateUserByEmail); //alternative admin update.
+router.delete('/:id',authenticate, userCtrl.removeUserById); // admin delete
 
 
 export default router;
