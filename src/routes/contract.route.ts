4c4aac87b1b487e63ad12a7e507439fbf14ff9a9
Alkapandelis <alexkpnd@gmail.com>
2026-01-11 01:56:20 +0200

    Feat:Add admin privileges to routes

--- src/routes/contract.route.ts
+++ src/routes/contract.route.ts
@@ -4,6 +4,7 @@ import { authenticate } from '../middlewares/auth.middleware';
 import { validate } from '../middlewares/validate.middleware';
 import { createContractSchema, updateContractSchema } from '../validators/contract.validator';
 import { validateObjId } from '../middlewares/validateObjId.middleware';
+import { hasAdminRole } from '../middlewares/userPriv.middleware';
 const router = Router();
 
 
@@ -11,6 +12,6 @@ router.get('/', authenticate,  contractCtrl.findAllContracts);
 router.get('/:id', authenticate, validateObjId("id"), contractCtrl.findContractById);
 router.post('/',authenticate, validate(createContractSchema), contractCtrl.createNewContract);
 router.put('/:id', authenticate, validateObjId('id'), validate(updateContractSchema), contractCtrl.updateContract); 
-router.delete('/:id', authenticate, validateObjId('id'), contractCtrl.removeContract); //admin delete
+router.delete('/:id', authenticate, hasAdminRole, validateObjId('id'), contractCtrl.removeContract); //admin delete
 
 export default router;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,6 +4,7 @@ import { validate } from '../middlewares/validate.middleware';
 import { createUserSchema, updateUserSchema } from '../validators/user.validator';
 import { validateObjId } from '../middlewares/validateObjId.middleware';
 import { authenticate } from '../middlewares/auth.middleware';
+import { hasAdminRole } from '../middlewares/userPriv.middleware';
 
 const router = Router();
 
@@ -12,10 +13,10 @@ const router = Router();
 
 router.get('/', authenticate, userCtrl.getAllUsers);
 router.get('/:id', authenticate, validateObjId('id'), userCtrl.getUserById);
-router.post('/', authenticate, validate(createUserSchema), userCtrl.createUser); //admin create
-router.put('/update/:id',authenticate, validateObjId('id'), validate(updateUserSchema),  userCtrl.updateUser); // admin update 
+router.post('/', authenticate, hasAdminRole, validate(createUserSchema), userCtrl.createUser); //admin create
+router.put('/update/:id',authenticate, hasAdminRole, validateObjId('id'), validate(updateUserSchema),  userCtrl.updateUser); // admin update 
 router.put('/updateme',authenticate, validate(updateUserSchema), userCtrl.updateMe);
-router.delete('/:id',authenticate, validateObjId('id'), userCtrl.removeUserById); // admin delete
+router.delete('/:id',authenticate, hasAdminRole, validateObjId('id'), userCtrl.removeUserById); // admin delete
 
 
 export default router;
